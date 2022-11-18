3f4dcb5b306c7833b412c4f7701c795eeb4fbb8f
Albert Lehninger <albert.lehninger1928@gmail.com>
2022-11-18 21:33:59 +0100

    animation added

--- src/components/NavBar/ShoppingCart.js
+++ src/components/NavBar/ShoppingCart.js
@@ -1,15 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartViewContext } from "../../Helpers/Context";
 
 const ShoppingCart = (props) => {
   const { setCartView } = useContext(CartViewContext);
+
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  useEffect(() => {
+    if (props.ItemCount === 0 || props.ItemCount === undefined) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [props.ItemCount])
+
+  const animation = `${btnIsHighlighted ? 'animate-pulse' : ''}`;
 
   return (
     <button
       onClick={() => setCartView(true)}
       href="#"
       className={
-        "flex items-center text-gray-300  bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-xl text-xl px-10 py-4 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-600 dark:focus:ring-gray-600 dark:border-gray-700"
+        "flex items-center text-gray-300  bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-xl text-xl px-10 py-4 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-600 dark:focus:ring-gray-600 dark:border-gray-700 " + animation
       }
     >
       Shopping Cart
