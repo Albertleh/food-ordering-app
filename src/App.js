76657192cb49e21d1d522cce075daaa4e9f5ff0f
Albert Lehninger <39347355+Albertleh@users.noreply.github.com>
2022-11-20 18:04:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { /* useState, */ useReducer } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 
 import NavBar from "./components/NavBar/NavBar";
@@ -7,7 +7,9 @@ import Cart from "./components/Cart/Cart";
 import { CartViewContext } from "./Helpers/Context";
 import BlurBackground from "./components/UI/BlurBackground";
 
+
 const reducer = (state, action) => {
+  // managing items that are displayed in the shopping cart 
   switch (action.type) {
     case "add-item":
       return { ...state, items: [...state.items, action.payload] };
@@ -16,6 +18,7 @@ const reducer = (state, action) => {
     case "set-items":
       return { ...state, items: action.payload };
 
+    // managing the item- counter of the shopping cart button
     case "add-total-quantity":
       return {
         ...state,
@@ -34,42 +37,55 @@ const reducer = (state, action) => {
   }
 };
 
+// This is just for avoiding typos 
+const ACTION = {
+  ADD_ITEM: 'add-item',
+  DELETE_ITEMS: 'delete-items',
+  SET_ITEMS: 'set-items',
+  ADD_TOTAL_QUANTITY: 'add-total-quantity',
+  SUBTRACT_TOTAL_QUANTITY: 'subtract-total-quantity',
+  ZERO_TOTAL_QUANTITY: 'zero-total-quantity',
+}
+
 function App() {
   const [state, dispatch] = useReducer(reducer, {
     items: [],
-    cartView: false,
     totalQuantity: 0,
   });
-  // for reference: the commented out lines accomplish the same thing the reducer does with useState hook 
+  // for reference: the commented out lines accomplish the same thing the reducer does with useState 
 
+  // boolean that tells, whether the shopping cart should be displayed 
+  // this isn't defined in the reducer since i was gonna pass it down gobally using useContext
   const [cartView, setCartView] = useState(false);
+
   // const [items, setItems] = useState([]);
   // const [totalQuantity, setTotalQuantity] = useState(0);
 
   const ItemAddingHandler = (item) => {
     //setTotalQuantity(totalQuantity + parseInt(item.quantity));
-    dispatch({ type: "add-total-quantity", payload: item.quantity })
+    dispatch({ type: ACTION.ADD_TOTAL_QUANTITY, payload: item.quantity })
     // setItems([...items, item]);
-    dispatch({ type: "add-item", payload: item });
+    dispatch({ type: ACTION.ADD_ITEM, payload: item });
   };
 
   const ItemDeletingHandler = (key) => {
     if (key === "DELETE_ALL") {
       // setItems([]);
-      dispatch({ type: "delete-items" });
+      dispatch({ type: ACTION.DELETE_ITEMS });
       //setTotalQuantity(0);
-      dispatch({ type: 'zero-total-quantity' });
+      dispatch({ type: ACTION.ZERO_TOTAL_QUANTITY });
       return;
     }
+    // newItems receives all items, that are still in the shopping cart after a deletion occured
     let newItems = state.items.filter((item) => {
       if (item.anotherkey === key) {
         // setTotalQuantity(totalQuantity - item.quantity);
-        dispatch({ type: 'subtract-total-quantity', payload: item.quantity })
+        dispatch({ type: ACTION.SUBTRACT_TOTAL_QUANTITY, payload: item.quantity })
       }
       return item.anotherkey !== key;
     });
     // setItems(newItems);
-    dispatch({ type: "set-items", payload: newItems });
+    dispatch({ type: ACTION.SET_ITEMS, payload: newItems });
   };
 
   return (
